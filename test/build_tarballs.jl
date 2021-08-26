5b0df6bb81d379d7e0f480e684cca9c18c99daa4
Steve Kelly <kd2cca@gmail.com>
2021-08-25 23:05:28 -0400

    [test] local build and test against loopback

---
+++ test/build_tarballs.jl
@@ -0,0 +1,47 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "SoapyLoopback"
+version = v"0.1.0"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/JuliaTelecom/SoapyLoopback.git", "9462da47c615598883d83d0530f01a76d112d01b")
+]
+
+dependencies = [
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae")),
+    Dependency(PackageSpec(name="soapysdr_jll", uuid="343a40d9-ed99-5d34-8b56-649aaa4ecee6"))
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd SoapyLoopback
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
+      -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+      -DCMAKE_BUILD_TYPE=Release \
+      ..
+make -j${nproc}
+make install
+if [[ "${target}" == *-apple-* ]]; then
+    # TODO: Rename
+    mv ${libdir}/SoapySDR/modules0.8/libsoapyloopback.so  ${libdir}/SoapySDR/modules0.8/libsoapyloopback.dylib
+fi
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = HostPlatform() #filter!(p -> arch(p) != "armv6l", supported_platforms(;experimental=true))
+platforms = expand_cxxstring_abis(platforms)
+
+# The products that we will ensure are always built
+products = Product[
+    LibraryProduct("libsoapyloopback", :librtlsdrSupport, ["lib/SoapySDR/modules0.8/"])
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+# gcc7 constraint from boost
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
