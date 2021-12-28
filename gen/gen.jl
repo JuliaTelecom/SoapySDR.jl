ae2ed21c3e37e9d4201273b9ace4a5d0dfdc2fd5
Steve Kelly <kd2cca@gmail.com>
2021-12-27 23:30:52 -0500

    ignore Logger as it hangs the build

--- gen/gen.jl
+++ gen/gen.jl
@@ -11,6 +11,8 @@ clang_dir = joinpath(include_dir, "clang-c")
 
 options = load_options(joinpath(@__DIR__, "generator.toml"))
 
+@show options
+
 # add compiler flags, e.g. "-DXXXXXXXXX"
 args = get_default_args()
 push!(args, "-I$include_dir")
@@ -18,6 +20,8 @@ push!(args, "-I$include_dir")
 headers = [joinpath(include_dir, "SoapySDR", header) for header in readdir(joinpath(include_dir, "SoapySDR")) if endswith(header, ".h")]
 # there is also an experimental `detect_headers` function for auto-detecting top-level headers in the directory
 # headers = detect_headers(clang_dir, args)
+
+filter!(s -> basename(s) != "Logger.h", headers) # ignore Logger as it hangs the build
 
 # create context
 ctx = create_context(headers, args, options)
