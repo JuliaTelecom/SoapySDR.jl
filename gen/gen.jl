8abd40d2705ff2e81d6f204b92e3c7023ca0375d
Steve Kelly <kd2cca@gmail.com>
2021-12-27 23:16:31 -0500

    clang: fix include path

--- gen/gen.jl
+++ gen/gen.jl
@@ -6,7 +6,7 @@ using soapysdr_jll
 
 using Clang.Generators
 
-include_dir = joinpath(soapysdr_jll.artifact_dir, "include", "SoapySDR") |> normpath
+include_dir = joinpath(soapysdr_jll.artifact_dir, "include") |> normpath
 clang_dir = joinpath(include_dir, "clang-c")
 
 options = load_options(joinpath(@__DIR__, "generator.toml"))
@@ -15,7 +15,7 @@ options = load_options(joinpath(@__DIR__, "generator.toml"))
 args = get_default_args()
 push!(args, "-I$include_dir")
 
-headers = [joinpath(include_dir, header) for header in readdir(include_dir) if endswith(header, ".h")]
+headers = [joinpath(include_dir, "SoapySDR", header) for header in readdir(joinpath(include_dir, "SoapySDR")) if endswith(header, ".h")]
 # there is also an experimental `detect_headers` function for auto-detecting top-level headers in the directory
 # headers = detect_headers(clang_dir, args)
 
