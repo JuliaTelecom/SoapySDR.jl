4a3314c39a87adddffc5b96a138ae7187b0ff75c
Elliot Saba <staticfloat@gmail.com>
2022-07-28 17:31:15 +0000

    Ensure that `read!(s::Stream, ...)` does not loop forever
    When the underlying hardware is not properly configured, we should not
    loop forever in `read!()`, especially as we are given a `timeout` value.
    If we time out, we should return.  We do not yet have a good way to
    surface these kinds of errors to the user, we may need to provide a
    different API (that returns error values, etc...) for more advanced
    usage.

--- src/error.jl
+++ src/error.jl
@@ -1,8 +1,29 @@
 
 """
     error_to_string(error)
+
+Calls `SoapySDR_errToStr(error)`, returning the string.
 """
 function error_to_string(error)
     ptr = SoapySDR_errToStr(error)
     ptr === C_NULL ? "" : unsafe_string(ptr) 
+end
+
+const SoapyStreamFlags = Dict(
+    "END_BURST" => SOAPY_SDR_END_BURST,
+    "HAS_TIME" => SOAPY_SDR_HAS_TIME,
+    "END_ABRUPT" => SOAPY_SDR_END_ABRUPT,
+    "ONE_PACKET" => SOAPY_SDR_ONE_PACKET,
+    "MORE_FRAGMENTS" => SOAPY_SDR_MORE_FRAGMENTS,
+    "WAIT_TRIGGER" => SOAPY_SDR_WAIT_TRIGGER,
+)
+
+function flags_to_set(flags)
+    s = Set{String}()
+    for (name, val) in SoapyStreamFlags
+        if val & flags != 0
+            push!(s, name)
+        end
+    end
+    return s
 end
